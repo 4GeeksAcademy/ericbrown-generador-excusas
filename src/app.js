5abb3094b96da08e97589f49dcae8820f471bc94
eric499 <100245477+eric499@users.noreply.github.com>
2025-07-05 03:17:02 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -8,4 +8,14 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+const whoRandom = who[Math.floor(Math.random() * who.length)]
+const actionRandom = action[Math.floor(Math.random() * action.length)]
+const whatRandom = what[Math.floor(Math.random() * what.length)]
+const whenRandom = when[Math.floor(Math.random() * when.length)]
+document.getElementById("excuse").innerHTML = whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom
 };
